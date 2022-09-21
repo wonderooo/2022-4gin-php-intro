f67c1c1e504fa86b004120b573cf02e49b730090
kacper szczesny <kacperek.szczesny@gmail.com>
2022-09-21 14:51:29 +0200

    fix render_csv

--- zad4.php
+++ zad4.php
@@ -1,18 +1,15 @@
 <?php
     function render_csv($array, $cols) {
         $csv_file = fopen('render.csv', 'w') or die('Unable to open file');
-        for ($i=0; $i<count($array)/$cols; $i++) {
-            $row = "";
-            for ($j=0; $j<$cols; $j++) {
-                $idx = $cols*$i+$j;
-                if ($idx < $cols*($i+1)-1 and $idx < count($array)) {
-                    $row = $row.$array[$idx].",";
-                } else if ($idx == $cols*($i+1)-1 and $idx < count($array)) {
-                    $row = $row.$array[$idx]."\n";
-                }
-            }
-            fwrite($csv_file, $row);
+        $text = "";
+        for ($n=1; $n<=count($array); $n++) {
+           if ($n % $cols == 0) {
+                $text = $text.$array[$n-1]."\n";
+           } else {
+                $text = $text.$array[$n-1].",";
+           }
         }
+        fwrite($csv_file, $text);
         fclose($csv_file);
     }
 
